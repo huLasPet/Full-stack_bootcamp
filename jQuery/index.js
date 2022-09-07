d80796110d2e63214ebab478a090ad73d6c989e8
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-09-07 12:57:08 +0200

    jQuery training

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,13 @@
 $("h1").css("color", "red");
+$("h1").toggleClass("jQueryTest");
+
+$("h1").on("click", testFunction);
+
+function testFunction() {
+  $("h1").css("color", "black");
+}
+
+$(document).on("keypress", function (event) {
+  $("h1").css("color", "blue");
+  $("h1").html(event.keysd);
+});
