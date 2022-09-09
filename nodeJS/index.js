80557ffb00a05677173f01f376d186cb0757b207
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-09-09 13:05:13 +0200

    node.JS start

---
+++ nodeJS/index.js
@@ -0,0 +1,5 @@
+var superHeroes = require("superheroes");
+var superVillains = require("supervillains");
+var randomHero = superHeroes.random();
+var randomVillain = superVillains.random();
+console.log(randomVillain + " beat the sh*t out of " + randomHero);
