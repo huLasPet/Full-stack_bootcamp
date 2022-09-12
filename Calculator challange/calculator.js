43295c92d746719c8ed1ebd1feda04e9fb9d8051
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-09-12 16:38:48 +0200

    Get data from form

--- Calculator challange/calculator.js
+++ Calculator challange/calculator.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/", (req, res) => {
+  let num1 = Number(req.body.num1);
+  let num2 = Number(req.body.num2);
+  let resultAddition = num1 + num2;
+  res.send("Result is " + resultAddition);
+});
 app.listen(3000);
