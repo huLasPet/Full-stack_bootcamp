cfb378d61dfbb25c53bf09478b561aa04725a276
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-09-12 18:43:26 +0200

    BMI calculator mini challange

--- Calculator challange/calculator.js
+++ Calculator challange/calculator.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Addition calculator part
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
@@ -12,4 +13,16 @@ app.post("/", (req, res) => {
   let resultAddition = num1 + num2;
   res.send("Result is " + resultAddition);
 });
+
+//BMI part
+app.get("/bmi", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("/bmi", (req, res) => {
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
+  let bmi = (weight / height / height) * 10000;
+  res.send("Your BMI is " + bmi.toFixed(2));
+});
+
 app.listen(3000);
