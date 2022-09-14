f8fa9c9807f9ada12ff0c4f6c111b2ae6c4819dd
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-09-14 20:01:50 +0200

    Added city and metric/imperial selector

--- Weather API/server.js
+++ Weather API/server.js
@@ -1,23 +1,31 @@
 const express = require("express");
 const https = require("node:https");
 const app = express();
-const apiKey = "&appid=1234";
-const units = "&units=metric";
+const apiKey = "&appid=";
 const weatherEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
-let city = "q=Budapest";
-let url = weatherEndpoint + city + apiKey + units;
-let weather;
+let celsiusOrNot;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/city", (req, res) => {
+  let city = "q=" + req.body.city;
+  let units = "&units=" + req.body.metric;
+  if (req.body.metric == "Metric") {
+    celsiusOrNot = "C";
+  } else {
+    celsiusOrNot = "F";
+  }
+  let url = weatherEndpoint + city + apiKey + units;
   https.get(url, (response) => {
     response.on("data", (d) => {
-      weather = JSON.parse(d);
+      let weather = JSON.parse(d);
       let weatherIcon = "http://openweathermap.org/img/wn/" + weather.weather[0].icon + "@4x.png";
-      console.log(weatherIcon);
-      res.write("<h1>It is currently " + weather.main.temp + " C in " + city.slice(2) + ".</h1>");
+      res.write("<h1>It is currently " + weather.main.temp + celsiusOrNot + " in " + city.slice(2) + ".</h1>");
       res.write("<p>The weather description currently: " + weather.weather[0].description + "</p>");
       res.write("<img src=" + weatherIcon + ">");
       res.send();
