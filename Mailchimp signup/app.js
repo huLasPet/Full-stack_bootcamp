1a664883f292461cbcab6b9e73c793e0db2c390c
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-09-15 10:20:30 +0200

    Mailchimp signup form done

--- Mailchimp signup/app.js
+++ Mailchimp signup/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const https = require("node:https");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); //To serve static files, path will be relative to this in the .html
 
 app.get("/", (req, res) => {
-  res.send("Works");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  console.log(firstName, lastName, email);
 });
 
 app.listen(3000);
