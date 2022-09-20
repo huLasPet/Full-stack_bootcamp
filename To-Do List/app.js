3493fa021342e69bb4daf67e41bab6e4e2fcf397
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-09-20 11:59:32 +0200

    EJS template learning

--- To-Do List/app.js
+++ To-Do List/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const ejs = require("ejs");
 const app = express();
+
+app.set("view engine", "ejs");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  let today = new Date();
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let whatDay = days[today.getDay()];
+  res.render("list", { day: whatDay });
 });
 
 app.listen(3000);
