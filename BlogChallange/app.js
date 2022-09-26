a0f3bcc33c399f1f5c32f0a108dc4acba95f0e56
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-09-26 10:20:35 +0200

    Added compose page

--- BlogChallange/app.js
+++ BlogChallange/app.js
@@ -13,9 +13,9 @@ const aboutText =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis facilisis tortor. Duis fermentum cursus convallis. Phasellus et eleifend libero. In at tortor vehicula, porta libero et, rutrum risus. Aliquam ultrices, mauris hendrerit fringilla scelerisque, massa nulla posuere urna, sit amet condimentum est lorem quis dolor. Vestibulum venenatis at ante et porta. Nam fermentum eros id ex pellentesque facilisis. Morbi sed varius dui. Nullam mattis nulla et turpis vestibulum, eget cursus felis mattis. Proin nisi enim, ornare eget eleifend quis, malesuada in neque. Sed faucibus iaculis cursus. Etiam hendrerit pharetra arcu non sagittis. Sed sit amet felis ornare, semper risus.";
 const contactText =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu nulla egestas, efficitur mi vitae, porttitor ex. Vivamus consequat erat aliquam faucibus elementum. Nunc congue quis lectus nec finibus.";
-
+let blogText = [];
 app.get("/", (req, res) => {
-  res.render("index", { indexText: homeStartingText });
+  res.render("index", { indexText: homeStartingText, blogText: blogText });
 });
 
 app.get("/about", (req, res) => {
@@ -24,5 +24,14 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.render("contact", { contactText: contactText });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("newblogpost");
+});
+
+app.post("/", (req, res) => {
+  blogText.push(req.body.blogPost);
+  res.redirect("/");
 });
 app.listen(3000);
