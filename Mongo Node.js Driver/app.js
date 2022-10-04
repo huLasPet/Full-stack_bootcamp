4860537579de6a6f36c91126cff22fe5fc53be49
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-10-04 10:58:54 +0200

    Create and delete entry

--- Mongo Node.js Driver/app.js
+++ Mongo Node.js Driver/app.js
@@ -13,8 +13,17 @@ async function run() {
     // Query for a movie that has the title 'Back to the Future'
     const query = { name: "pencil2" };
     const product = await products.findOne(query);
-
     console.log(product);
+
+    //Add an entry to the DB
+    const doc = { name: "Neapolitan pizza", price: 20 };
+    const result = await products.insertOne(doc);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+
+    //Delete the above entry
+    const deleteEntry = { _id: result.insertedId };
+    const deleteResult = await products.deleteOne(deleteEntry);
+    console.log(`A deleted successfully: ${deleteResult.acknowledged}`);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
