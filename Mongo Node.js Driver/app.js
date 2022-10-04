7ac6c23565a0ee84e15e0a7ab13b2f7cb10f46b8
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-10-04 10:44:12 +0200

    Mongo driver test

---
+++ Mongo Node.js Driver/app.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+
+// Replace the uri string with your connection string.
+const uri = "mongodb://localhost:27017";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const database = client.db("shopDB");
+    const products = database.collection("products");
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { name: "pencil2" };
+    const product = await products.findOne(query);
+
+    console.log(product);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
