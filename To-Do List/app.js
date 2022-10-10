ec57781eabe8e4b5a9096d9fd8e88d99fe52c4b2
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-10-10 14:42:53 +0200

    Upgraded ToDo to use Mongoose

--- To-Do List/app.js
+++ To-Do List/app.js
@@ -1,23 +1,62 @@
 const express = require("express");
 const ejs = require("ejs");
 const customDate = require("./date");
+const mongoose = require("mongoose");
+
+//Setting up the app
 const app = express();
-
 app.set("view engine", "ejs");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let toDoList = ["Food", "More food", "Fooooood"];
-
-app.get("/", (req, res) => {
-  res.render("list", { day: customDate.getTheDay(), toDoList: toDoList });
+//Creating the Schema and Model for Mongoose
+const toDoSchema = new mongoose.Schema({
+  todo: String,
+  date: String,
 });
+const toDoModel = mongoose.model("ToDo", toDoSchema);
 
-app.post("/", (req, res) => {
-  toDoList.push(req.body.newToDo);
-  res.redirect("/");
-});
+//Create the list to hold the items from the DB
+let toDoList = [];
 
-app.listen(3000);
+//Connect to the DB and add a new ToDo item from req
+async function addToDB(req) {
+  await mongoose.connect("mongodb://localhost:27017/todoDB");
+  let toDoItem = new toDoModel({ todo: req.body.newToDo, date: customDate.getTheDay() });
+  await toDoItem.save();
+  mongoose.connection.close();
+}
+
+//Get all ToDos from the DB
+async function getFromDB() {
+  await mongoose.connect("mongodb://localhost:27017/todoDB");
+  toDoList.length = 0;
+  let allToDos = await toDoModel.find();
+  allToDos.forEach((element) => {
+    toDoList.push(element);
+  });
+  mongoose.connection.close();
+}
+
+function main() {
+  app.get("/", async (req, res) => {
+    //Get data from the DB, if that was successful THEN call the anonym function to render the page
+    //.then({success}, {reject}) is the full but there is no reject here
+    getFromDB().then(() => {
+      res.render("list", { day: customDate.getTheDay(), toDoList: toDoList });
+    });
+  });
+
+  app.post("/", async (req, res) => {
+    //Add the data to the DB and when that is done THEN call the redirect
+    //Nothing on reject here either
+    addToDB(req).then(() => {
+      res.redirect("/");
+    });
+  });
+
+  app.listen(3000);
+}
+
+main();
