dc653db92f5b253de966807d5f3bff0c25e04244
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-11-04 13:16:55 +0100

    Added packages

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
+const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//Creating the Schema and Model for Mongoose
+const SecretsSchema = new mongoose.Schema({
+  secret: String,
+  user: String,
+});
+const SecretsModel = mongoose.model("Secrets", SecretsSchema);
+
 function main() {
   app.get("/", (req, res) => {
     res.render("home");
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("login");
+  });
+
+  app.get("/register", (req, res) => {
+    res.render("register");
   });
 
   app.listen(3000);
