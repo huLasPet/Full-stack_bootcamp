ee2264f667c7a4a35ff718310aca45aa67796ffb
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-11-11 13:32:41 +0100

    Passport initial

---
+++ Secrets/app_passport.js
@@ -0,0 +1,77 @@
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const app = express();
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//Creating the Schema and Model for Mongoose
+const SecretsSchema = new mongoose.Schema({
+  secret: String,
+  user: String,
+});
+const SecretsModel = mongoose.model("Secret", SecretsSchema);
+
+const UserSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+//Adding encryption using mongoose-encrypt, only encrypting the password so can still search based on e-mail
+UserSchema.plugin(encrypt, { secret: process.env.SECRETS_PROJECT_SECRET, encryptedFields: ["password"] });
+
+const UserModel = mongoose.model("User", UserSchema);
+
+async function getOneFromDB(username) {
+  await mongoose.connect("mongodb://localhost:27017/userDB");
+  let oneUser = await UserModel.findOne({ email: username });
+  mongoose.connection.close();
+  return oneUser;
+}
+
+async function addToDB(email, password) {
+  await mongoose.connect("mongodb://localhost:27017/userDB");
+  let newUser = new UserModel({ email: email, password: password });
+  //With this it returns true if it saved and returns the error if it didn't - still need to use return result at the end
+  let result = await newUser
+    .save()
+    .then(() => {
+      return "True";
+    })
+    .catch((err) => {
+      console.log("Something went wrong: ", err.message);
+      return err.message;
+    });
+  mongoose.connection.close();
+  return result;
+}
+
+function main() {
+  app.get("/", (req, res) => {
+    res.render("home");
+  });
+
+  app
+    .route("/login")
+    .get((req, res) => {
+      res.render("login");
+    })
+    .post((req, res) => {});
+
+  app
+    .route("/register")
+    .get((req, res) => {
+      res.render("register");
+    })
+    .post((req, res) => {});
+
+  app.listen(3000);
+}
+
+if (require.main === module) {
+  main();
+}
