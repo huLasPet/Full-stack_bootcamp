3e0dee060b52d9efffe1e1b306f28dcc1b4565a4
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-11-23 13:48:30 +0100

    React training start

---
+++ React/React_1/react_1/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <h1>Test</h1>
+    <h2>Test2</h2>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </div>
+);
