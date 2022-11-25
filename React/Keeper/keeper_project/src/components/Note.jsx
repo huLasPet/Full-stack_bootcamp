dd9aac8433bb25a064436e32eeef13a3791c8ab3
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-11-25 15:34:17 +0100

    Keeper project - added Notes section and styling

--- React/Keeper/keeper_project/src/components/Note.jsx
+++ React/Keeper/keeper_project/src/components/Note.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
 function Note() {
-  return (
-    <div className="notes">
-      <h1>Notes come here</h1>
-    </div>
-  );
+  let notes = [];
+  for (let i = 0; i < 5; i++) {
+    notes.push(
+      <div class="col-sm-4">
+        <div className="card">
+          <div className="card-body">
+            <h4>Notes come here</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return notes;
 }
 
 export { Note };
