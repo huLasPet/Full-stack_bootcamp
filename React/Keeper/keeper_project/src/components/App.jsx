21de3ee1c5b8952dd406ca686fc8827ac0886658
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-11-28 14:23:12 +0100

    Keeper project .map practice

--- React/Keeper/keeper_project/src/components/App.jsx
+++ React/Keeper/keeper_project/src/components/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { Header } from "./Header.jsx";
 import { Note } from "./Note.jsx";
 import { Footer } from "./Footer.jsx";
+import { notes } from "./Notes.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Note title="Note title" text="Note text" />
-      <Note title="Note title 2" text="Note text 2" />
+      {/* Map is the same as forEach - would be better here but this is practice.
+       Need to add the key as well even if not displayed otherwise it will throw errors */}
+      {notes.map((props) => {
+        return <Note key={props.key} title={props.title} text={props.text} />;
+      })}
       <Footer />
     </div>
   );
--- React/Keeper/keeper_project/src/components/Note.jsx
+++ React/Keeper/keeper_project/src/components/Note.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Note(props) {
   return (
-    <div class="col-sm-4">
+    <div className="col-sm-4">
       <div className="card">
         <div className="card-body">
           <h4>{props.title}</h4>
