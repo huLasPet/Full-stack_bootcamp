6455501bb93b1163d4bfd52a6b7141b56573e478
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-11-30 15:18:53 +0100

    React states training

--- React/React_States/state_test/src/App.js
+++ React/React_States/state_test/src/App.js
@@ -4,17 +4,23 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  let [stateCount, setState] = React.useState(0);
-  function increase() {
-    setState(stateCount + 1);
+  //Display the current time, update automatically every second or can click to update as well
+  //Start with empty stateTime in useState but after 1 second the setTime() is triggered to give it a value
+  let [stateTime, setState] = React.useState();
+  function setTime() {
+    let currentTime = new Date();
+    setState(currentTime.toLocaleTimeString());
+    //Use clearInterval() to ensure there is only 1 setInterval is running - stop a memory leak
+    clearInterval();
   }
+  setInterval(setTime, 1000);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>{stateCount}</h1>
-        <button onClick={increase}>+</button>
+        <h1>{stateTime}</h1>
+        <button onClick={setTime}>Get time</button>
       </header>
     </div>
   );
