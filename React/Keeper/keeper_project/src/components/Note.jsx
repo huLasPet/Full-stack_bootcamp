aab3458a60e46048a9bdabf0c7f28ca21ea5433d
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-12-02 14:37:05 +0100

    State practice with click action

--- React/Keeper/keeper_project/src/components/Note.jsx
+++ React/Keeper/keeper_project/src/components/Note.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Note(props) {
+  const [fontColor, setFontColor] = useState("black");
+
+  let divStyle = {
+    color: fontColor,
+  };
+
+  function changeColor() {
+    if (fontColor === "red") {
+      setFontColor("black");
+    } else {
+      setFontColor("red");
+    }
+  }
+
   return (
     <div className="col-sm-3">
       <div className="card">
         <div className="card-body">
-          <h4>{props.title}</h4>
+          <h4 onClick={changeColor} style={divStyle}>
+            {props.title}
+          </h4>
           <p>{props.text}</p>
         </div>
       </div>
