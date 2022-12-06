b69267375dda73057f3776fe5110de81756b6560
huLasPet <92375480+huLasPet@users.noreply.github.com>
2022-12-06 11:22:25 +0100

    React state changing practice

--- React/Keeper/keeper_project/src/components/Header.jsx
+++ React/Keeper/keeper_project/src/components/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
   return (
     <div className="headerDiv">
       <h1>Keeper</h1>
+      <button className="btn btn-outline-light btn-sm">Add note</button>
     </div>
   );
 }
--- React/React_States/state_test/src/App.js
+++ React/React_States/state_test/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   let [stateTime, setState] = React.useState(new Date().toLocaleTimeString());
@@ -20,12 +21,45 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
+  //Display name typed into the form
+  let [fName, setfName] = useState("");
+  let [lName, setlName] = useState("");
+
+  function updatefName(event) {
+    if (event.nativeEvent.data === null) {
+      setfName(fName.slice(0, -1));
+    } else {
+      setfName(fName + event.nativeEvent.data);
+    }
+  }
+
+  function updatelName(event) {
+    if (event.nativeEvent.data === null) {
+      setlName(lName.slice(0, -1));
+    } else {
+      setlName(lName + event.nativeEvent.data);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>{stateTime}</h1>
         <button onClick={setTime}>Get time</button>
+
+        <h1>-----------</h1>
+
+        <div className="container">
+          <h1>
+            Hello {fName} {lName}
+          </h1>
+          <form>
+            <input onChange={updatefName} name="fName" placeholder="First Name" />
+            <input onChange={updatelName} name="lName" placeholder="Last Name" />
+            <button>Submit</button>
+          </form>
+        </div>
       </header>
     </div>
   );
